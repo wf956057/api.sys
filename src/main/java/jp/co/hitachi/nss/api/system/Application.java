e255c6edb204706baff6310e35144ab6c60441f5
wf956057 <wf956057@github.com>
2020-03-03 14:32:42 +0900

    upload

---
+++ src/main/java/jp/co/hitachi/nss/api/system/Application.java
@@ -0,0 +1,13 @@
+package jp.co.hitachi.nss.api.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
